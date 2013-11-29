272cd4d68cc65bf1dc37c8dfef5ce952ab67755a
Akaname <antonio.verlotta@gmail.com>
2013-11-29 09:30:25 +0900

    first commit

---
+++ goldenGate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  goldenGate
+//
+//  Created by Antonio on 11/29/13.
+//  Copyright (c) 2013 D.Sco.Ver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ goldenGate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  goldenGate
+//
+//  Created by Antonio on 11/29/13.
+//  Copyright (c) 2013 D.Sco.Ver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
